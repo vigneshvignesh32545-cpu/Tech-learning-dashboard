b801b3ae4facc392c29fd25db4f81db7d5a36e57
Vignesh K <vigneshvignesh32545@gmail.com>
2026-07-01 23:44:27 +0530

    Updated dashboard

--- pages/1_📊_Analytics.py
+++ pages/1_📊_Analytics.py
@@ -1,3 +1,37 @@
 import streamlit as st
 
 st.title("📊 Analytics")
+
+course_count = df["Course"].value_counts().reset_index()
+course_count.columns = ["Course", "Students"]
+
+fig = px.bar(
+        course_count,
+        x="Course",
+        y="Students",
+        title="Students by Course"
+    )
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig2 = px.pie(
+        course_count,
+        names="Course",
+        values="Students",
+        title="Course Popularity"
+    )
+
+st.plotly_chart(fig2, use_container_width=True)
+
+status_count = df["Status"].value_counts().reset_index()
+status_count.columns = ["Status", "Count"]
+
+fig3 = px.pie(
+        status_count,
+        names="Status",
+        values="Count",
+        hole=0.5,
+        title="Student Status"
+    )
+
+st.plotly_chart(fig3, use_container_width=True)
--- pages/2_👨‍🎓_Students.py
+++ pages/2_👨‍🎓_Students.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
 st.title("👨‍🎓 Students")
+
+
+
+
+st.subheader("📋 Student Details")
+
+search = st.text_input("🔍 Search Course")
+
+filtered_df = df
+if search:
+        filtered_df = filtered_df[
+            filtered_df["Course"].str.contains(search, case=False)
+            |
+            filtered_df["Trainer"].str.contains(search, case=False)
+            |
+            filtered_df["Status"].str.contains(search, case=False)
+        ]
+
+st.dataframe(filtered_df)
